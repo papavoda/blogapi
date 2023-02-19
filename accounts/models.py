d8cca418476436dfd40a828e73105a43daf9ebf4
papavoda <papavoda@gmail.com>
2023-02-19 11:08:32 +0300

    Blog API setup

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
